803c72bc68bd49750f85322db328a73f5afcf4ce
Mizael Carlos <mizaelcarlos44@gmail.com>
2021-05-16 16:38:04 -0300

    inserção de imagem na tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,11 +2,14 @@
 
 @section('content')
 <div class="container">
+
+		<img src="{{asset('/img/UniEAD.jpg')}}" class="rounded float-left">
+				
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
-
+				
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
